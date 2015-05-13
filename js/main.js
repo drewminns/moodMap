77985077b14a0a79ddae4929258f9dd01b81122f
drewminns <drew.minns@me.com>
2015-05-13 14:53:40 -0400

    Added deletion

--- js/main.js
+++ js/main.js
@@ -5,12 +5,14 @@ picApp.locals = {
 	context  : canvas.getContext('2d'),
 	video 	 : document.getElementById('video'),
 	videoObj : { "video" : true },
+	token : '10d27ca9d306973',
 	ffpkey  : '14c4a74157f02f6421c02ce63b7e09ed',
 	ffpscrt : 'JHy_J7A-liJmBitvTEKq4Z6zlcXbsUs3',
+	imgurImg : '',
 	errBack  : function(err) {
 		console.log("Video Capture Error: ", error.code);
 	}
-}
+};
 
 picApp.canvas = function() {
 	if (navigator.getUserMedia) {
@@ -24,12 +26,18 @@ picApp.canvas = function() {
 			picApp.locals.video.play();
 			picApp.stream = stream;
 		}, picApp.locals.errBack);
+	} else if (navigator.mozGetUserMedia) {
+		navigator.mozGetUserMedia(picApp.locals.videoObj, function(stream) {
+			picApp.locals.video.src = window.URL.createObjectURL(stream);
+			picApp.locals.video.play();
+			picApp.stream = stream;
+		}, picApp.locals.errBack);
 	}
-}
+};
 
 picApp.pic = function() {
 	$('#snap').on('click', function() {
-		picApp.locals.context.drawImage(video, 0, 0, 320, 240);
+		picApp.locals.context.drawImage(video, 0, 0, 640, 480);
 		picApp.locals.video.pause();
 		picApp.stream.stop();
 		picApp.locals.vibePhoto = picApp.convert(picApp.locals.canvas);
@@ -37,24 +45,23 @@ picApp.pic = function() {
 		$('.preview').html(picApp.locals.vibePhoto).addClass('show');
 		picApp.upload();
 	});
-}
+};
 
 picApp.convert = function(canvas) {
 	var image = new Image();
 	image.src = canvas.toDataURL("image/jpeg");
 	return image;
-}
+};
 
 picApp.upload = function() {
 	var $img = $('img');
-	var token = '10d27ca9d306973';
 	localStorage.doUpload = true;
 	localStorage.imageBase64 = $img.attr('src').replace(/.*,/, '');
 	$.ajax({
 	  url: 'https://api.imgur.com/3/image',
 	  method: 'POST',
 	  headers: {
-	    Authorization: 'Client-ID ' + token,
+	    Authorization: 'Client-ID ' + picApp.locals.token,
 	    Accept: 'application/json'
 	  },
 	  data: {
@@ -62,12 +69,29 @@ picApp.upload = function() {
 	    type: 'base64'
 	  },
 	  success: function(result) {
+	  	picApp.deleteLink = result.data.deletehash;
 	    var id = result.data.id;
 	    var link = 'http://i.imgur.com/' + id + '.png';
+	    picApp.locals.imgurImg = result.data.id;
 	    picApp.getEmotions(link);
 	  }
 	});
-}
+};
+
+picApp.deletePic = function(info) {
+	$.ajax({
+		url: 'https://api.imgur.com/3/image/' + info,
+		method: 'DELETE',
+		dataType: 'json',
+		headers: {
+		  Authorization: 'Client-ID ' + picApp.locals.token,
+		  Accept: 'application/json'
+		},
+		success: function(rez) {
+			console.log(rez);
+		}
+	});
+};
 
 picApp.getEmotions = function(image) {
 	$.ajax({
@@ -80,20 +104,30 @@ picApp.getEmotions = function(image) {
     },
     success: function(data) {
     	picApp.sortData(data.face[0]);
+    	picApp.deletePic(picApp.deleteLink);
+    },
+    error: function(rez) {
+    	console.log(rez);
     }
 	});
-}
+};
+
+
+
+
 
 picApp.sortData = function(info) {
 	console.log(info);
 	$('<p class="gender"></p>').text(info.attribute.gender.value).appendTo('.preview');
-}
+	$('<p class="age"></p>').text(info.attribute.age.value).appendTo('.preview');
+	$('<p class="race"></p>').text(info.attribute.race.value).appendTo('.preview');
+};
 
 picApp.init = function() {
 	picApp.canvas();
 	picApp.pic();
-}
+};
 
 $(function() {
-		picApp.init();
+		// picApp.init();
 });
